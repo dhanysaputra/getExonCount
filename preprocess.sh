ba22c313bca8374bc6b8a27f1b65d9e81c74d450
dhany <dhany.saputra@ki.se>
2014-01-10 06:51:38 +0100

    revision on 'for loop'

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 # command: bash preprocess.sh yourfile.bam mapqcutoff maxchr_nonXY outputprefix
-# example: bash preprocess.sh srr28-29.bam 30 22 human1
+# example: bash preprocess.sh srr28-29.bam 30 22 tumor
 samtools view -F 4 $1 | awk -v mapq=$2 '{ if ($5>=mapq && $7=="=") print $0 } ' | sort -k2,2 -t '.' > deleteme.sam
 for i in {1..$((${3}+1))}; do grep -P "\tchr$i\t" deleteme.sam > $4.chr$i; done
 grep -P '\tchrX\t' deleteme.sam > $4.chrX
