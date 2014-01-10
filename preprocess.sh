ba699efd94953fd79e84fdccbe767d6193c8016a
dhany <dhany.saputra@ki.se>
2014-01-10 06:53:04 +0100

    revision on 'for loop' and module addition

--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,11 @@
+
+module load bioinfo-tools
+module load samtools/0.1.18
+
 # command: bash preprocess.sh yourfile.bam mapqcutoff maxchr_nonXY outputprefix
 # example: bash preprocess.sh srr28-29.bam 30 22 tumor
 samtools view -F 4 $1 | awk -v mapq=$2 '{ if ($5>=mapq && $7=="=") print $0 } ' | sort -k2,2 -t '.' > deleteme.sam
-for i in {1..$((${3}+1))}; do grep -P "\tchr$i\t" deleteme.sam > $4.chr$i; done
+for (( i=1; i<=$(( $3+1 )); i++ )); do grep -P "\tchr$i\t" deleteme.sam > $4.chr$i; done
 grep -P '\tchrX\t' deleteme.sam > $4.chrX
 grep -P '\tchrY\t' deleteme.sam > $4.chrY
 rm -f deleteme.sam
